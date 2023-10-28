9065b95bdaed2d688acb9390617f302ba5013a86
이기운 <slehdejfj@gmail.com>
2023-10-28 12:39:29 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts}"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.text-stroke': {
+          '-webkit-text-stroke-width': '2px',
+          '-webkit-text-stroke-color': '#000',
+        },
+        '.text-transparent-fill': {
+          '-webkit-text-fill-color': 'transparent',
+        },
+      };
+      addUtilities(newUtilities, ['responsive', 'hover']);
+    },
+  ],
+}
